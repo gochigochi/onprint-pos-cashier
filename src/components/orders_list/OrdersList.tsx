f28a07a8c03b34a90e5cb9d64e1bd1b680f26d04
diego <diegoeliseoiovane@gmail.com>
2024-03-25 06:53:53 -0300

    order list - list format

--- src/components/orders_list/OrdersList.tsx
+++ src/components/orders_list/OrdersList.tsx
@@ -1,40 +1,63 @@
-import { useState } from "react"
 import { useGetOrdersQuery } from "../../store/api/apiSlice"
+import { formatDate } from "../../utils/formatDate"
 
-const OrdersList = () => {
+const OrdersList = ({ page, setHasNextPage, setLoading }) => {
 
-    const [page, setPage] = useState(1)
-    const { data, error, isLoading } = useGetOrdersQuery({ page: page })
+    const { data, error, isFetching, isLoading } = useGetOrdersQuery({ page: page })
 
     //case not found -404- or sorts
     if (error) throw Error
+
+    if (data?.hasNextPage) {
+        setHasNextPage(true)
+    } else {
+        setHasNextPage(false)
+    }
+
+    if (isFetching) {
+        setLoading(true)
+    }
+    else {
+        setLoading(false)
+    }
 
     console.log(data)
 
     return (
         <div>
-            <div>
-                <button onClick={() => setPage(prev => prev - 1)}>prev</button>
-                <p>{page}</p>
-                <button onClick={() => setPage(prev => prev + 1 )}>next</button>
-            </div>
-            <div className="flex flex-col divide-y-2">
-                {
-                    !isLoading ?
-                    data?.orders.map(order => {
-                        return (
-                            <div key={order.id} className="py-5 px-1 flex gap-4">
-                                <div className="font-semibold">{order.number}</div>
-                                <div>{order.id}</div>
-                                <div>{order.shipping.first_name}</div>
-                                <div>{order.status}</div>
-                                <div>CHF. {order.total}</div>
-                            </div>
-                        )
-                    }) : 
-                    <p>loading....</p>
-                }
-            </div>
+            <table className="w-full">
+                <thead className="sticky -top-6 grid grid-cols-12 gap-2 bg-white p-4 shadow-md shadow-slate-100">
+                    <tr className="col-span-1 ">Order</tr>
+                    <tr className="col-span-4">Customer</tr>
+                    <tr className="col-span-2">Date</tr>
+                    <tr className="col-span-2">Status</tr>
+                    <tr className="col-span-2">Total</tr>
+                </thead>
+                <tbody className={`flex flex-col divide-y divide-slate-200 px-4 first-line:selection:${isFetching && "opacity-45"}`}>
+                    {
+                        !isLoading ?
+                            data?.orders.map(order => {
+                                return (
+                                    <tr key={order.id} className="col-span-1 py-5 grid grid-cols-12 gap-2">
+                                        <td className="font-semibold">{order.number}</td>
+                                        <td className="col-span-4 overflow-hidden whitespace-nowrap text-ellipsis w-[85%]">{order.shipping.first_name}</td>
+                                        <td className="col-span-2">{formatDate(order.date_created)}</td>
+                                        <td className="col-span-2">{order.status}</td>
+                                        <td className="col-span-2">CHF. {order.total}</td>
+                                        <td>
+                                            <button>
+                                                <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                                                </svg>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                )
+                            }) :
+                            <p>loading....</p>
+                    }
+                </tbody>
+            </table>
         </div>
     )
 }
--- src/pages/orders/Orders.tsx
+++ src/pages/orders/Orders.tsx
@@ -1,18 +1,29 @@
+import { useState } from "react"
 import ErrorBoundary from "../../components/error_boundary/ErrorBoundary"
 import ErrorFallback from "../../components/error_fallback/ErrorFallback"
 import OrdersList from "../../components/orders_list/OrdersList"
+import Pagination from "../../components/pagination/Pagination"
 
 const Orders = () => {
 
+    const [page, setPage] = useState(1)
+    const [hasNextPage, setHasNextPage] = useState(true)
+    const [loading, setLoading] = useState(false)
     const errorMsg = "An error ocurred fetching orders"
 
     return (
         <div>
-            <h2 className="section-title">Orders</h2>
-            <div className="bg-white shadow-md p-4 rounded-md mt-4 min-h-[28rem]">
+            <div className="flex justify-between items-center z-20">
+                <h2 className="section-title">Orders</h2>
+                <Pagination page={page} setPage={setPage} hasNextPage={hasNextPage} loading={loading} />
+            </div>
+            <div className="relative bg-white shadow-md rounded-md my-4 min-h-[50rem]">
                 <ErrorBoundary fallback={<ErrorFallback>{errorMsg}</ErrorFallback>}>
-                    <OrdersList />
+                    <OrdersList page={page} setHasNextPage={setHasNextPage} setLoading={setLoading} />
                 </ErrorBoundary>
+            </div>
+            <div className="flex justify-end items-center">
+                <Pagination page={page} setPage={setPage} hasNextPage={hasNextPage} loading={loading} />
             </div>
         </div>
     )
